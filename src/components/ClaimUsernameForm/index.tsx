c84769242844f3f792c7b672761981a035cf40fc
Christian Chang Thomaz <christianchangthomaz@gmail.com>
2025-11-03 18:01:39 -0300

    Add username claiming functionality and handle username routing

--- src/components/ClaimUsernameForm/index.tsx
+++ src/components/ClaimUsernameForm/index.tsx
@@ -5,6 +5,7 @@ import { Form, FormAnnotation } from "./styles";
 import z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
 
 const ClaimUsernameFormSchema = z.object({
   username: z
@@ -22,13 +23,17 @@ export function ClaimUsernameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(ClaimUsernameFormSchema),
   });
 
+  const router = useRouter();
+
   async function handleClaimUsername(data: ClaimUsernameFormData) {
-    console.log(data);
+    const { username } = data;
+
+    router.push(`/register?username=${username}`);
   }
 
   return (
@@ -41,7 +46,7 @@ export function ClaimUsernameForm() {
           placeholder="seu-usuário"
           {...register("username")}
         />
-        <Button size="sm" type="submit">
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
